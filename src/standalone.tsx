22db7e08917b4e58c6fe7ea9833e0770455bbed4
Podyl <75058807+Podyl@users.noreply.github.com>
2026-09-04 20:58:54 +0200

    Use polished animated brawler build

--- src/standalone.tsx
+++ src/standalone.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { PhaserBrawlerFixed } from "./components/game/PhaserBrawlerFixed";
+import { PhaserBrawlerPro } from "./components/game/PhaserBrawlerPro";
 import "./styles.css";
 import "./phaser-brawler.css";
 
-// Runtime-tested Phaser build entry.
 createRoot(document.getElementById("root")!).render(
-  <PhaserBrawlerFixed />,
+  <PhaserBrawlerPro />,
 );
