e80146dbb2a801bfe30429b716ff622f0be9a9ee
Podyl <75058807+Podyl@users.noreply.github.com>
2026-09-04 20:26:59 +0200

    Use fixed Phaser controls and sprite loader

--- src/standalone.tsx
+++ src/standalone.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { PhaserBrawler } from "./components/game/PhaserBrawler";
+import { PhaserBrawlerFixed } from "./components/game/PhaserBrawlerFixed";
 import "./styles.css";
 import "./phaser-brawler.css";
 
 createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <PhaserBrawler />
-  </React.StrictMode>,
+  <PhaserBrawlerFixed />,
 );
